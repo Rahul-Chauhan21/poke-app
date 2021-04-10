ab17e8eeeb013e430cf60daeb9ff0435e39fe3e2
Rahul-Chauhan21 <crahul1721@gmail.com>
2021-04-10 14:33:53 +0530

    Updated Poke route

--- poke-backend/services/pokemon.js
+++ poke-backend/services/pokemon.js
@@ -1,29 +1,33 @@
 const superagent = require("superagent");
 
-const getPokemon = (id) => {
-  return new Promise((resolve) => {
-    superagent
-      .get(`https://pokeapi.co/api/v2/pokemon/${id}/`)
-      .then((response) => {
-        const pokemon = response.body;
-        response.body = {
-          name: pokemon.name,
-          types: pokemon.types.map((t) => t.type.name),
-          sprite: pokemon.sprites.front_default,
+const getPokemon = async (id) => {
+  try {
+    const res = await superagent.get(
+      `https://pokeapi.co/api/v2/pokemon/${id}/`
+    );
+    return res;
+  } catch (err) {
+    return { name: "notfound" };
+  }
+};
+const Pokemon = async (req, res) => {
+  const response = await Promise.all(
+    req.body.ids.map((id) => getPokemon(id))
+  ).then((result) => {
+    return result
+      .filter((el) => el !== null)
+      .map((el) => {
+        if (el.name === "notfound") {
+          return { name: "notfound" };
+        }
+        return {
+          name: el.body.name,
+          types: el.body.types.map((t) => t.type.name),
+          sprite: el.body.sprites.front_default,
         };
-        resolve(response.body);
-      })
-      .catch((err) => {
-        errorDetails = { name: "notfound" };
-        resolve(errorDetails);
       });
   });
-};
-
-const Pokemon = async (req, res) => {
-  Promise.all(req.body.ids.map((id) => getPokemon(id)))
-    .then((result) => res.send(result))
-    .catch((err) => res.send(`Error occurred: ${err}`));
+  res.status(200).json(response);
 };
 
 module.exports = Pokemon;
